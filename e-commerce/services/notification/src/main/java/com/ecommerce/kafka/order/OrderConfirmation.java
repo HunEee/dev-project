bc461c910ee22e6627ddc3c4a652d8f858f4e7fd
HunEee <asdf981001@naver.com>
2025-12-26 14:09:54 +0900

    ecommerce: notification 카프카 기본 세팅

---
+++ e-commerce/services/notification/src/main/java/com/ecommerce/kafka/order/OrderConfirmation.java
@@ -0,0 +1,17 @@
+package com.ecommerce.kafka.order;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.ecommerce.kafka.payment.PaymentMethod;
+
+public record OrderConfirmation(
+        String orderReference,
+        BigDecimal totalAmount,
+        PaymentMethod paymentMethod,
+        Customer customer,
+        List<Product> products
+
+) {
+	
+}
